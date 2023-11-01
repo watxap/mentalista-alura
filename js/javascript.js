59d35b3c1bea38757aa99217811ddeca6116c58d
Matias Paz <neoxap@gmail.com>
2023-11-01 14:53:39 -0300

    Resolución desafío 2

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,27 @@
 var numeroSecreto = parseInt(Math.random() * 1000);
-var intentosN = 0
+var intentos = 0
+var intentosMax = prompt(`Te doy la bienvenida al MENTALISTA, un juego donde deberás adivinar un número secreto entre el 1 al 1000.
+Vamos a definir la dificultad: por favor, ingresá el número de intentos para adivinar el número misterioso:`)
+
+
 
 while (numeroDigitado != numeroSecreto) {
-  var numeroDigitado = prompt(`Ingrese un número entre 1 y 1000. El número de intentos actual es de ${intentosN}.`);
-  var intentosN = intentosN+1
-  //si el numeroDigitado es igual al número secreto
-  if (numeroDigitado == numeroSecreto) {
+  var numeroDigitado = prompt(`Ingrese un número entre 1 y 1000. Te quedan ${intentosMax-intentos} intentos extra.`);
+  //es igual a volver a declarar la variable sumándole 1:
+  intentos += 1;
+  if ((intentosMax-intentos)==-1) {
+    alert ('Lo siento, superaste el número de intentos máximo. Vuelve a intentarlo.')
+    break;
+  }
+  //si el numeroDigitado es igual al número secreto:
+  if (numeroDigitado == null) {
+    alert("Has cancelado el juego. ¡Hasta luego!");
+    break;
+  } else if (numeroDigitado == numeroSecreto) {
     alert('¡Acertaste!');
   } else if (numeroDigitado > numeroSecreto) {
     alert('Te equivocaste... el número secreto es menor');
   } else if (numeroDigitado < numeroSecreto) {
     alert('Te equivocaste... el número secreto es mayor');
   }
-}
+} 
