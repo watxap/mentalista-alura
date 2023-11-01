93c0ca4979dc38d7b3ff946d46b37eb02e7cd72d
Matias Paz <neoxap@gmail.com>
2023-11-01 02:23:24 -0300

    Resolución primer desafío

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,9 @@
 var numeroSecreto = parseInt(Math.random() * 1000);
+var intentosN = 0
 
-while(numeroDigitado != numeroSecreto) {
-  var numeroDigitado = prompt('Ingrese un número entre 1 y 1000');
+while (numeroDigitado != numeroSecreto) {
+  var numeroDigitado = prompt(`Ingrese un número entre 1 y 1000. El número de intentos actual es de ${intentosN}.`);
+  var intentosN = intentosN+1
   //si el numeroDigitado es igual al número secreto
   if (numeroDigitado == numeroSecreto) {
     alert('¡Acertaste!');
